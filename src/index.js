ca95606cc382ca08e2accfbc99d947dad5844e42
Hugo <hugo@thegonzalez.design>
2018-01-12 17:44:04 -0800

    Removed cors, renamed routes, changed the port and added handle args

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const morgan = require('morgan')
 const server = express()
 
-const public = '../client/public/'
-
 server.use(morgan('combined'))
 server.use(bodyParser.json())
-server.use(cors())
-server.use(express.static(public))
 server.set('trust proxy', true)
 server.set('trust proxy', 'loopback')
 
-server.get('/getData', (req, res) => {
-	res.send(handle.get())
+server.get('/get', (req, res) => {
+	res.send(handle.get(req.body.query))
 })
 
-server.post('/publish', (req, res) => {
-	res.send(handle.publish({
+server.post('/post', (req, res) => {
+	res.send(handle.post({
 		"data": req.body.data
 	}))
 })
 
-server.listen(8081)
+server.listen(3000)
