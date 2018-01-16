d16a5e226b3dda644f39d75f1d79dfe25ead9e80
Hugo <hugo@thegonzalez.design>
2018-01-15 22:44:15 -0800

    modified schema and testing handle

--- src/api/handles/find.js
+++ src/api/handles/find.js
@@ -1,9 +1,30 @@
 const {
 	wrapper
 } = require('./re/wrapper')
-const {
-	query
-} = require('./re/query')
-exports.handle = wrapper((item, callback) => {
-	item.meta.model.find(query(item), callback)
+exports.handle = wrapper((meta) => {
+	let _res = 'Nothing!'
+	meta.model.find(meta.query)
+		.then((res) => {
+			if (res) {
+				console.log(res)
+				_res = res
+			}
+		})
+		.catch((err) => {
+			if (err) console.error('Error:', err)
+		})
+	return _res
 })
+
+exports.schema = {
+	'route': 'find',
+	'type': 'item',
+	'data': {
+		'!meta': {
+			'!query': {
+				'!key': 'name'
+			}
+		}
+	},
+	'!metaOnly': true
+}
