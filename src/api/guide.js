bc22d33c5a2b010c8832b1ca8fc44974bf81f933
Hugo <hugo@thegonzalez.design>
2018-01-15 22:42:58 -0800

    modified to return a 'response' and to use 'metaOnly'

--- src/api/guide.js
+++ src/api/guide.js
@@ -1,15 +1,21 @@
 const {
 	handles
 } = require('./handles')
+
+const {
+	respond
+} = require('./respond')
+
 exports.guide = (req) => {
-	const data = req.body.data
-	const route = req.body.route
-	const type = req.body.type
+	const route = req.route
+	const type = req.type
 	const handle = handles[route]
+	let data = req.data
+	data['metaOnly'] = req['metaOnly'] && true
 	if (typeof handle === 'function') {
 		return handle(data, type)
 	} else {
 		console.error('Error:', `The route [${route}] doesn't exist`)
-		return [false]
+		return respond()
 	}
 }
