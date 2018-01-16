322f8c77ebf5c2885f4edf10cf0cb791b4fccbcd
Hugo <hugo@thegonzalez.design>
2018-01-15 22:45:39 -0800

    includes metaOnly option

--- src/api/handles/re/wrapper.js
+++ src/api/handles/re/wrapper.js
@@ -1,40 +1,52 @@
 const {
 	respond
-} = require('./respond')
+} = require('../../respond')
 exports.wrapper = (actionOn) => {
-	const unwrapped = (data, type) => {
+	const unwrapped = async (data, type) => {
 		let responses = []
 		let _Model
 		try {
 			const {
 				Model
-			} = require(`../models/${type}`)
+			} = require(`../../models/${type}`)
 			_Model = Model
 		} catch (error) {
 			console.error(`Error: The model [${type}] doesn't exist`)
 			responses.push(respond(false))
 			return responses
 		}
-		let callback = (error, data) => {
-			if (error) {
-				console.error('Error:', error.errmsg)
-				return respond(false)
-			} else {
-				console.log(data)
-				return respond(true, data)
-			}
-		}
-		data.forEach(async (item) => {
-			item.meta['actual'] = new _Model(item.data)
-			item.meta['model'] = _Model
+		console.log('\nData:', data)
+		console.log('\n')
+		if (data['metaOnly']) {
+			let meta = data.meta
+			meta['model'] = _Model
 			try {
-				const result = await actionOn(item, callback)
+				const result = await actionOn(meta)
+				console.log('Response', result)
 				responses.push(result)
 			} catch (error) {
 				console.error(error)
 				responses.push(respond(false))
 			}
-		})
+		} else {
+			if (data) {
+				data.forEach(async (item) => {
+					item.meta['actual'] = new _Model(item.data)
+					item.meta['model'] = _Model
+					try {
+						const result = await actionOn(item)
+						responses.push(result)
+					} catch (error) {
+						console.error(error)
+						responses.push(respond(false))
+					}
+				})
+			} else {
+				console.error('Error:', 'data is invalid')
+				console.log('Data:\n', data)
+				responses.push(respond(false))
+			}
+		}
 		return responses
 	}
 	return unwrapped
