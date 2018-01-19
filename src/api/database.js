cb91b6ac0a0364670ba1cf263464e4eab9289dc1
Hugo <hugo@thegonzalez.design>
2018-01-18 23:47:27 -0800

    added try catch statements

--- src/api/database.js
+++ src/api/database.js
@@ -1,13 +1,25 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-exports.connect = (db = 'index') => {
-	mongoose.connect(`mongodb://localhost/${db}`, {
-		useMongoClient: true
-	})
-	mongoose.connection.once('open', function () {
-		console.log(`Connection to [${db}] has been made. Hazaah!`)
-	}).on('error', function (error) {
-		console.error('Connection error', error)
-	})
+exports.connect = async (db = 'index') => {
+	try {
+		await mongoose.connect(`mongodb://localhost/${db}`, {
+			useMongoClient: true
+		})
+		mongoose.connection.once('open', () => {
+			console.log(`Connection to [${db}] has been made. Hazaah!`)
+		}).on('error', (error) => {
+			console.error('Connection error', error)
+		})
+	} catch (error) {
+		console.error('Error:', `Connection to database failed`)
+	}
+}
+
+exports.disconnect = async () => {
+	try {
+		await mongoose.disconnect()
+	} catch (error) {
+		console.error('Error:', `Disconnection to database failed`)
+	}
 }
