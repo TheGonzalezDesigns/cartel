8b1a9f4e42033b0b2b045142facea9d6f7ca6c5a
Hugo <hugo@thegonzalez.design>
2018-01-18 23:48:43 -0800

    fixed a few typos and misplacements of _p

--- src/api/validate.js
+++ src/api/validate.js
@@ -3,65 +3,73 @@ const {
 	schema
 } = require('./schemas')
 
-Object.compare = function (obj1, obj2) {
-	let _p
+Object.compare = function (obj1, obj2, verbose = true) {
 	let strict = false
 	// Loop through properties in object 1
 	for (let p in obj1) {
-		strict = p.slice(0, 1) === '!'
-		_p = strict ? p.slice(1) : p
-		if (strict) console.log('\nStrict mode found!')
+		const strict = p.slice(0, 1) === '!'
+		const _p = strict ? p.slice(1) : p
+		console.log(`Comparing ${_p}:\n\tobj1:\t${obj1[p]}\n\tobj2:\t${obj2[_p] || obj2[p]}`)
+		if (strict) {
+			if (verbose) console.log('\nStrict mode found!')
+		}
 		// Check property exists on both objects
-		console.log('\nChecking property exists on both objects...')
-		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(_p)) return false
+		if (verbose) console.log('\nChecking property exists on both objects...')
+		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(_p)) {
+			console.log(`Property missing, expected ${p} from ${obj2[_p]}`)
+			return false
+		}
 
 		if (obj1[p] === null && obj2[_p] !== null) return false
-		if (obj2[p] === null && obj1[_p] !== null) return false
-		console.log('Passed')
-		console.log('Now deeply comparing', _p)
+		if (obj2[_p] === null && obj1[p] !== null) return false
+		if (verbose) console.log('Passed')
+		if (verbose) console.log('Now deeply comparing', _p)
 		switch (typeof (obj1[p])) {
 			// Deep compare objects
 			case 'object':
-				console.log('Entering sub-object...')
-				if (!Object.compare(obj1[p], obj2[_p])) return false
+				if (verbose) console.log('Entering sub-object...')
+				if (!Object.compare(obj1[p], obj2[_p])) {
+					console.log('Failed @l27')
+					return false
+				}
 				break
 				// Compare function code
 			case 'function':
-				console.log('Evaluating function...')
+				if (verbose) console.log('Evaluating function...')
 				if (typeof (obj2[_p]) === 'undefined' || (p !== 'compare' && obj1[p].toString() !== obj2[_p].toString())) return false
 				break
 				// Compare values
 			default:
-				console.log('Comparing values...')
-				console.log('Checking for empty values...')
+				if (verbose) console.log('Comparing values...')
+				if (verbose) console.log('Checking for empty values...')
 				if (obj1[p] === '' && obj2[_p] !== '') return false
-				if (obj2[p] === '' && obj1[_p] !== '') return false
-				console.log('No empty values found')
+				if (obj2[_p] === '' && obj1[p] !== '') return false
+				if (verbose) console.log('No empty values found')
 				// Tests for equality if the key contains an '!' appended as
 				if (strict) {
-					console.log('Testing values for equality')
+					if (verbose) console.log('Testing values for equality')
 					if (obj1[p] !== obj2[_p]) return false
-					console.log(`Values are equivalent: [${obj1[p]}, ${obj2[_p]}]`)
-				} else console.log('Currently valid...')
+					if (verbose) console.log(`Values are equivalent: [${obj1[p]}, ${obj2[_p]}]`)
+				} else if (verbose) console.log('Currently valid...')
 		}
-		console.log(`\n${_p} Passed\n`)
+		if (verbose) console.log(`\n${_p} Passed\n`)
 	}
 
 	// Check object 2 for any extra properties
 	if (strict) {
-		console.log('Checking object 2 for any extra properties')
+		if (verbose) console.log('Checking object 2 for any extra properties')
 		for (let p in obj2) {
-			console.log('Searching for', p)
+			if (verbose) console.log('Searching for', p)
 			let variatA = obj1[('!' + p)]
 			let variatB = obj1[p]
 			let testA = typeof (variatA) === 'undefined'
 			let testB = typeof (variatB) === 'undefined'
 
 			if (testA && testB) return false
-			console.log(`[${p}] found.`)
+			if (verbose) console.log(`[${p}] found.`)
 		}
 	}
-	console.log('Testing ended as a success')
+	if (verbose) console.log('Testing ended as a success')
 	return true
 }
 
